b10978db494330abc3e2ec8b29e069fad80af7c0
Oleksandr <telyuksasha@gmail.com>
2023-11-17 18:28:24 +0200

    Fixed api error

--- locustfile.py
+++ locustfile.py
@@ -1,6 +1,15 @@
 from locust import FastHttpUser, task, between
 
-endpoints = ['/api/actors', '/api/awards', '/api/companies', '/api/countries', '/api/directors', '/api/genres', '/api/movies', '/api/nominations', '/api/roles']
+endpoints = ['/api/actors',
+             '/api/awards',
+             '/api/companies',
+             '/api/countries',
+             '/api/directors',
+             '/api/genres',
+             '/api/movies',
+             '/api/nominations',
+             '/api/roles']
+
 
 class Test(FastHttpUser):
     wait_time = between(1, 2)
@@ -8,7 +17,7 @@ class Test(FastHttpUser):
     @task
     def cloud_load(self):
         for endpoint in endpoints:
-            self.client.get(f"{endpoint}", auth=('user','password'))
+            self.client.get(f"{endpoint}", auth=('user', 'password'))
         self.client.get(f"{endpoints[3]}/2")
         for id in range(6, 9):
-            self.client.get(f"{endpoints[6]}{id}")
+            self.client.get(f"{endpoints[6]}/{id}")
